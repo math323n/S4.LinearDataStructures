e1d763b72c5f29c879365a7aa235beaa2fea8aed
math323n <math323n@edu.campusvejle.dk>
2021-02-02 15:00:09 +0100

    added hanoi towers (not optimized

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -45,5 +45,26 @@ namespace ConsoleApp
             }
             return true;
         }
+
+
+        // unoptimized code
+        public static void TowerOfHanoi(Stack<char> stack)
+        {
+            Move(stack.Count, 'A', 'C', 'B');
+        }
+
+        public static void Move(int n, char from_rod, char to_rod, char aux_rod)
+        {
+            if(n == 1)
+            {
+                Console.WriteLine("Move disk 1 from rod " +
+                                  from_rod + " to rod " + to_rod);
+                return;
+            }
+            Move(n - 1, from_rod, aux_rod, to_rod);
+            Console.WriteLine("Move disk " + n + " from rod " +
+            from_rod + " to rod " + to_rod);
+            Move(n - 1, aux_rod, to_rod, from_rod);
+        }
     }
 }   
